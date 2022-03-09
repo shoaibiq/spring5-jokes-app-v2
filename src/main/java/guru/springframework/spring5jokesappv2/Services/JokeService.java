df4ef7f593f6f3fc70596cea92f1e631e351d1f8
shoaib iqbal <shoaib.iqbal118@gmail.com>
2022-03-09 14:53:48 +0500

    # Addition of jokes service is done. Point 3 of the task is completed

---
+++ src/main/java/guru/springframework/spring5jokesappv2/Services/JokeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5jokesappv2.Services;
+
+import org.springframework.stereotype.Service;
+
+public interface JokeService {
+
+    public String getJokeString();
+//       return guru.springframework.norris.chuck.ChuckNorrisQuotes.getRandomQuote();
+
+}
