22969672e0696d6c1c0b719503d9039a01914a8c
shoaib iqbal <shoaib.iqbal118@gmail.com>
2022-03-09 17:41:21 +0500

    Completed points 4,5 and 6

---
+++ src/main/java/guru/springframework/spring5jokesappv2/Controllers/JokesController.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring5jokesappv2.Controllers;
+
+import guru.springframework.spring5jokesappv2.Services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+    private final JokeService jokeService;
+
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJokeString());
+        return "index";
+    }
+}
